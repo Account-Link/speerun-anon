2fb6e74e4f4fcf5b8b26a4caaa628c869b362a95
Shreyas Londhe <62744899+shreyas-londhe@users.noreply.github.com>
2024-12-21 20:47:07 +0530

    Feat: Integrating JWT Verifier to AnonRedeem (#1)
    * chore: added prettierrc
    
    * feat: prover integration done
    
    * feat: added JWT Verifier to anonredeem
    
    * lint avoid
    
    ---------
    
    Co-authored-by: Andrew Miller <socrates1024@gmail.com>

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,9 +27,8 @@ export default function RootLayout({
   return (
     <Web3Provider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <script src="https://accounts.google.com/gsi/client" async defer></script>
           {children}
         </body>
       </html>
---
+++ frontend/pages/api/generateCircuitInputs.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { generateJWTAuthenticatorInputs } from "@zk-email/jwt-tx-builder-helpers/dist/input-generators";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    const { jwt, pubkey, maxMessageLength } = req.body;
+
+    if (!jwt || !pubkey || !maxMessageLength) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const circuitInputs = await generateJWTAuthenticatorInputs(jwt, pubkey, BigInt(0), {
+      maxMessageLength,
+      exposeGoogleId: true,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { accountCode, codeIndex, iatKeyStartIndex, ...filteredCircuitInputs } = circuitInputs;
+
+    res.status(200).json(filteredCircuitInputs);
+  } catch (error) {
+    console.error("Error generating circuit inputs:", error);
+    res.status(500).json({ error: "Failed to generate inputs" });
+  }
+}
